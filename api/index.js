33ee929db9a45b254535989bfc9bbab26130c1d9
anmedina-arg <andres.medina.arg@gmail.com>
2024-08-14 11:35:39 -0300

    antes de deploy en railway

--- api/index.js
+++ api/index.js
@@ -1,14 +1,13 @@
 //import funcion-que-carga-paises-en-BD from ("./funciones")
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-//const { PORT } = process.env;
+const { PORT } = process.env;
 const { uploadCountries } = require("./src/functions/uploadCountries")
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(PORT, () => {
     uploadCountries// <-- llamar a la función que carga todos los paises en la BD
-    console.log(`%s listening at 3001`);
-    //console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
 });
