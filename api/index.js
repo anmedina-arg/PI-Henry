ddc6e59e07cb3c83938b1a7afd2f294bdd5f9ea1
anmedina-arg <andres.medina.arg@gmail.com>
2024-08-14 17:43:00 -0300

    modifico de nuevo el index.js lo dejo como al principio

--- api/index.js
+++ api/index.js
@@ -1,18 +1,13 @@
 //import funcion-que-carga-paises-en-BD from ("./funciones")
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
-require('dotenv').config();
 const { PORT } = process.env;
-const { uploadCountries } = require("./src/functions/uploadCountries");
+const { uploadCountries } = require("./src/functions/uploadCountries")
 
 // Syncing all the models at once.
-conn.sync({ force: true })
-  .then(() => {
-    server.listen(PORT, "0.0.0.0", () => {
-      uploadCountries(); // Llamar a la función que carga todos los países en la BD
-      console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
-    });
-  })
-  .catch(err => {
-    console.error('Error syncing database:', err); // Manejo de errores al sincronizar la base de datos
+conn.sync({ force: true }).then(() => {
+  server.listen(PORT, () => {
+    uploadCountries// <-- llamar a la función que carga todos los paises en la BD
+    console.log(`%s listening at ${PORT}`); // eslint-disable-line no-console
   });
+});
