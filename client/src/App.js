db2a5bc17376333d9a093e2a450be134c8480cec
anmedina-arg <andres.medina.arg@gmail.com>
2024-08-15 09:00:49 -0300

    agrego endpoint del back desplegado en render

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ import Home from './components/home';
 
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3001/api'
+axios.defaults.baseURL = 'https://pi-henry-cujj.onrender.com/api'
 
 
 function App() {
